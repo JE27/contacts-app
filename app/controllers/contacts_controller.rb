ac057713a76687ba77702cbefa88b01fc45d0024
je27 <jocmoya@gmail.com>
2018-01-22 19:41:04 -0600

    migration

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,44 @@
 class ContactsController < ApplicationController
-  def one_contact_method
-    contact = Contact.last
-    render json: contact.as_json
-  end
-  
-  def all_contacts_method
+  def index
     contacts = Contact.all
     render json: contacts.as_json
   end
+
+  def create 
+    contacts = Contact.new(
+                           first_name: params[:first_name],
+                           middle_name: params[:first_name],
+                           last_name: params[:last_name],
+                           email: params[:phone_number],
+                           phone_number: params[:email],
+                           bio: params[:bio]
+                          )  
+    contacts.save
+    render json: contacts.as_json
+  end
+
+  def show
+    contacts = Contact.find(params[:id])
+    render json: contacts.as_json 
+  end
+
+  def update
+    contacts = Contact.find(params[:id])
+
+    contacts.first_name = params[:first_name] || contacts.first_name
+    contacts.middle_name = params[:middle_name] || contacts.middle_name
+    contacts.last_name = params[last_name] || contacts.last_name
+    contacts.phone_number = params[phone_number] || contacts.phone_number
+    contacts.email = params[email] || contacts.email
+    contacts.bio = params[bio] || contacts.bio
+
+    render json: contacts.as_json
+  end
+
+  def destroy
+    contacts = contacts.find(params[:id])
+    contacts.destroy
+    # render json: {message: "You've successfully deleted #{{contacts.first_name}} from your life."} 
+  end
+
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,29 @@
 class Contact < ApplicationRecord
+
+  def full_name
+   "#{first_name}#{last_name}".titleize
+
+  end
+
+  
+
+  def as_json
+      {
+       id: id,
+       full_name: full_name,
+       middle_name: middle_name,
+       first_name: first_name,
+       last_name: last_name,
+       email: email,
+       phone_number: phone_number,
+       updated_at: friendly_updated_at,
+       bio: bio
+      }  
+  end
+
+  def friendly_updated_at
+   updated_at.strftime("%b %d, %Y")
+  end
+
+  
 end
